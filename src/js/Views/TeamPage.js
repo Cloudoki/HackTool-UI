c99d5a4a9e8d15838f6a4b89601235aab50c7ac9
Rui Molefas <molefas@gmail.com>
2016-07-29 17:25:55 +0100

    added content to team page

--- src/js/Views/TeamPage.js
+++ src/js/Views/TeamPage.js
@@ -5,17 +5,33 @@ define(
 	{
 		var TeamPage = BaseView.extend({
 
-			events: {},	
+			events: {},
+			id: 'teampage',
+
+			teamMembers: [],
 
 			initialize: function(options) {
 
 			},	
 
-		    render: function()
+		    render: function(refresh)
 		    {	
-		    	this.$el.html(Mustache.render(Templates.team_page, {}));
+		    	this.$el.html(Mustache.render(Templates.team_page, {teamMembers: this.teamMembers}));
+
+		    	if (!refresh)
+		    		this.getTeam();
 
 		        return this;
+		    },
+
+		    getTeam: function() {
+
+		    	hacktoolSdk.Organizations.Teams.list("2052463", function(teamMembers) {
+		    		
+		    		this.teamMembers = teamMembers;
+		    		this.render(true);
+
+		    	}.bind(this));
 		    }
 		});
 
