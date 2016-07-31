1727e935886760f7311c042fa8c31082d171d836
Rui Molefas <molefas@gmail.com>
2016-07-31 21:09:29 +0100

    updated add repo to toolbelt action

--- src/js/Views/ToolBelt.js
+++ src/js/Views/ToolBelt.js
@@ -8,17 +8,20 @@ define(
 		var ToolBelt = BaseView.extend({
 
 			events: {
-				'click [data-action=edit]': 'edit',
-				'click [data-action=remove]': 'removeRepo'
+				'click [data-action=edit]': 'toggleEdit',
+				'click [data-action=done]': 'toggleEdit',
+				'click [data-action=remove]': 'removeRepo',
+				'focus .repo-add input': 'enableSubmit',
+				'blur .repo-add input': 'disableSubmit'
 			},	
 
 			initialize: function(options) {
 
 			},	
 
-		    render: function(refresh)
+		    render: function(refresh, edit)
 		    {	
-		    	this.$el.html(Mustache.render(Templates.tool_belt, {items: this.toolBelt}));
+		    	this.$el.html(Mustache.render(Templates.tool_belt, {items: this.toolBelt, edit: edit}));
 
 		    	if (!refresh)
 		    		this.getData();
@@ -35,9 +38,40 @@ define(
 				}.bind(this), this.error);
 		    },
 
-		    edit: function() {
+		    enableSubmit: function(e) {
 
-		    	this.$el.find('.tabbed-widget').addClass('edit');
+		    	$(e.currentTarget).keypress(function(key){
+		    		if (key.which == 13)
+		    			this.addRepo()
+		    	}.bind(this));
+		    },
+
+		    disableSubmit: function(e) {
+
+		    	$(e.currentTarget).off('keypress');
+		    },
+
+		    toggleEdit: function() {
+
+		    	this.$el.find('.tabbed-widget').toggleClass('edit');
+		    },
+
+		    addRepo: function() {
+
+		    	var name = this.$el.find('[data-attr=repo-name]').val();
+		    	var uri = this.$el.find('[data-attr=repo-uri]').val();
+
+		    	if (!name || !uri)
+		    		this.$el.find('.repo-add').append('nope.')
+
+		    	else {
+		    		this.toolBelt.push({name: name, url: uri});
+		    		this.toolBelt.sort(function(a,b){ return a.name > b.name });
+
+		    		this.save(function(){
+		    			this.render(true, true);
+		    		}.bind(this));
+		    	}
 		    },
 
 		    removeRepo: function(e) {
@@ -52,16 +86,18 @@ define(
 		    		}
 		    	}
 
-		    	this.save($item);
+		    	this.save(function(){
+		    		$item.remove()
+		    	});
 		    },
 
-		    save: function(item) {
+		    save: function(success, error) {
 
 		    	hacktoolSdk.Components.update('toolbelt', {
 		    		content: this.toolBelt,
 		    		sha: this.sha
 		    	}, function(a){
-		    		item.remove();
+		    		success()
 		    	});
 		    }
 		});
